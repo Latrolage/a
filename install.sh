3118e40589a068e55e9d57c3a3728b1e690fadf8
Latrolage <firecatmon@protonmail.com>
2021-10-27 23:24:05 +1100

    a

---
+++ install.sh
@@ -0,0 +1,7 @@
+killall gimp &
+wget -O ./photogimp.zip "https://github.com/Diolinux/PhotoGIMP/releases/download/1.0/PhotoGIMP.by.Diolinux.v2020.for.Flatpak.zip" -o ./photogimp.zip
+unzip ./photogimp.zip
+mv "PhotoGIMP by Diolinux v2020 for Flatpak" ./photogimp
+rm -rf ~/.config/GIMP
+cp ./photogimp/.var/app/org.gimp.GIMP/GIMP ~/.config/ -r
+cp ./photogimp/.var/app/org.gimp.GIMP/gtk-2.0/gtkfilechooser.ini ~/.config/gtk-2.0/
