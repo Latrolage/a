84ac92947bc26b7e1b9f5175d4008cdeff4b1482
Latrolage <67372293+Latrolage@users.noreply.github.com>
2021-12-04 17:50:40 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
+yay -Syu kdenlive gimp firefox okular libreoffice-fresh --needed
+
+
 killall gimp &
 wget -O ./photogimp.zip "https://github.com/Diolinux/PhotoGIMP/releases/download/1.0/PhotoGIMP.by.Diolinux.v2020.for.Flatpak.zip" -o ./photogimp.zip
-unzip ./photogimp.zip
+echo Unzipping...
+unzip -qq ./photogimp.zip
 mv "PhotoGIMP by Diolinux v2020 for Flatpak" ./photogimp
 rm -rf ~/.config/GIMP
 cp ./photogimp/.var/app/org.gimp.GIMP/config/GIMP ~/.config/ -r
