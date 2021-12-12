a08c7889c855a7baa397bff01971096849832cbb
Latrolage <67372293+Latrolage@users.noreply.github.com>
2021-12-13 00:19:22 +1100

    Update LSNew.sh

--- LSNew.sh
+++ LSNew.sh
@@ -2,15 +2,17 @@ sudo true
 curl -s https://raw.githubusercontent.com/linux-surface/linux-surface/master/pkg/keys/surface.asc \
     | sudo pacman-key --add -
 sudo pacman-key --finger 56C464BAAC421453
-sleep 5
+sleep 3
 sudo pacman-key --lsign-key 56C464BAAC421453
 sudo echo "[linux-surface]" >> /etc/pacman.conf
 sudo echo "Server = https://pkg.surfacelinux.com/arch/" >> /etc/pacman.conf
-sudo pacman -Syu linux-surface linux-surface-headers iptsd
+yay -Syu linux-surface linux-surface-headers iptsd gwenview mpv kdenlive gimp firefox okular libreoffice-fresh --needed --noconfirm
 sudo systemctl enable iptsd
+yay -Rns kdeconnect --noconfirm
 sudo sed -i '/#MAKEFLAGS=/c\MAKEFLAGS="-j$(nproc)"' /etc/makepkg.conf
-
 echo Done
 echo reminder: GRUB_DEFAULT=(index starts with 0)
 echo "/boot/grub/grub.cfg"
-sleep 100
+echo optionally https://addons.mozilla.org/en-US/firefox/addon/arch-linux-aur-search-webext/
+echo reminder: make libreoffice not look shit
+sleep 1000
